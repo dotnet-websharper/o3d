4b0222df80033290f99bbb0c5d77725b44ecf598
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-10-06 12:28:20 +0200

    Update for WebSharper 3.5 + Use F# 3.0

--- build.fsx
+++ build.fsx
@@ -4,6 +4,7 @@ open IntelliFactory.Build
 let bt =
     BuildTool().PackageId("WebSharper.O3D")
         .VersionFrom("WebSharper")
+        .WithFSharpVersion(FSharpVersion.FSharp30)
         .WithFramework(fun fw -> fw.Net40)
 
 let main =
@@ -14,7 +15,11 @@ let main =
 let test =
     bt.WebSharper.HtmlWebsite("WebSharper.O3D.Tests")
         .SourcesFromProject()
-        .References(fun r -> [r.Project main])
+        .References(fun r ->
+            [
+                r.Project main
+                r.NuGet("WebSharper.Html").Reference()
+            ])
 
 bt.Solution [
     main
