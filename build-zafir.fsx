bcc598ed7e6e9cee32f3d1db24967866724ca498
István Gansperger <gansperger@gmail.com>
2015-12-11 19:23:26 +0100

    Add zafir build

---
+++ build-zafir.fsx
@@ -0,0 +1,38 @@
+#load "tools/includes.fsx"
+open IntelliFactory.Build
+
+let bt =
+    BuildTool().PackageId("Zafir.O3D")
+        .VersionFrom("Zafir")
+        .WithFSharpVersion(FSharpVersion.FSharp30)
+        .WithFramework(fun fw -> fw.Net40)
+
+let main =
+    bt.Zafir.Extension("WebSharper.O3D")
+        .Embed(["o3d.js"])
+        .SourcesFromProject()
+
+let test =
+    bt.Zafir.HtmlWebsite("WebSharper.O3D.Tests")
+        .SourcesFromProject()
+        .References(fun r ->
+            [
+                r.Project main
+                r.NuGet("Zafir.Html").Reference()
+            ])
+
+bt.Solution [
+    main
+    test
+
+    bt.NuGet.CreatePackage()
+        .Configure(fun c ->
+            { c with
+                Title = Some "Zafir.O3D-20100829"
+                LicenseUrl = Some "http://websharper.com/licensing"
+                ProjectUrl = Some "https://github.com/intellifactory/websharper.o3d"
+                Description = "Zafir Extensions for O3D 20100829"
+                RequiresLicenseAcceptance = true })
+        .Add(main)
+]
+|> bt.Dispatch
