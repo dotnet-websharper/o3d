8737b69cb1a880b37960ccdfd2f1b6b3a7ded212
Jand42 <andras.janko@gmail.com>
2017-09-05 21:49:44 +0200

    Rename Zafir packages to WebSharper

--- build-zafir.fsx
+++ build-zafir.fsx
@@ -2,23 +2,23 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("Zafir.O3D")
-        .VersionFrom("Zafir")
+    BuildTool().PackageId("WebSharper.O3D")
+        .VersionFrom("WebSharper")
         .WithFSharpVersion(FSharpVersion.FSharp30)
         .WithFramework(fun fw -> fw.Net40)
 
 let main =
-    bt.Zafir.Extension("WebSharper.O3D")
+    bt.WebSharper4.Extension("WebSharper.O3D")
         .Embed(["o3d.js"])
         .SourcesFromProject()
 
 let test =
-    bt.Zafir.HtmlWebsite("WebSharper.O3D.Tests")
+    bt.WebSharper4.HtmlWebsite("WebSharper.O3D.Tests")
         .SourcesFromProject()
         .References(fun r ->
             [
                 r.Project main
-                r.NuGet("Zafir.Html").Latest(true).ForceFoundVersion().Reference()
+                r.NuGet("WebSharper.Html").Latest(true).ForceFoundVersion().Reference()
             ])
 
 bt.Solution [
@@ -28,10 +28,10 @@ bt.Solution [
     bt.NuGet.CreatePackage()
         .Configure(fun c ->
             { c with
-                Title = Some "Zafir.O3D-20100829"
+                Title = Some "WebSharper.O3D-20100829"
                 LicenseUrl = Some "http://websharper.com/licensing"
                 ProjectUrl = Some "https://github.com/intellifactory/websharper.o3d"
-                Description = "Zafir Extensions for O3D 20100829"
+                Description = "WebSharper Extensions for O3D 20100829"
                 RequiresLicenseAcceptance = true })
         .Add(main)
 ]
